296da59d11f785d3ee5d81b6e89dd6f521ce62ff
Laila-Haddad <108059233+Laila-Haddad@users.noreply.github.com>
2024-04-19 12:22:03 +0300

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# LemonPie
- A website for LemonPie, a world renouned bakery. made with HTML, CSS, Vanilla Js
+## Chef's Lemon Pie
+Dessert shop/restaurant website. Not a facade for money laundering and drug distribution.
+### Links
+- Live site: [Chef's Lemon Pie](https://laila-haddad.github.io/university-ass/dessert-restawranteh/home.html "Chef's Lemon Pie")
+- Source code: [Chef's Lemon Pie code](https://github.com/Laila-Haddad/university-ass/tree/main/dessert-restawranteh "Chef's Lemon Pie code")
---
+++ home.js
@@ -0,0 +1,111 @@
+
+window.onload = () => {
+  activeNav();
+}
+
+window.onscroll = () => {
+  activeNav();
+};
+
+
+//nav 
+function activeNav() {
+  const section = document.querySelectorAll(".content > div");
+  const menu = document.querySelectorAll("header nav li a");
+  var scrolly = window.scrollY;
+  // var fooTop= document.getElementById('Food').offsetTop - 80;
+  var abTop= document.getElementById('About').offsetTop - 80;
+
+  section.forEach((i) => {
+   
+    var top = i.offsetTop - 80;
+    var height = i.offsetHeight;
+    var id = i.getAttribute("id");
+
+
+    if (scrolly >= top && scrolly < top + height) {
+      menu.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav li a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+    else {
+      if (scrolly <=abTop) {
+        menu.forEach((link) => {link.classList.remove("active");});
+        document
+        .getElementsByClassName('home')[0]
+        .classList.add("active");
+      }
+    }
+  });
+
+
+  if (scrolly >abTop - 300){
+    document.getElementsByTagName('nav')[0].style.backgroundColor='var(--Grey)';
+    document.getElementById('Logo').style.backgroundSize='60%';
+
+    document.getElementsByTagName('nav')[0].style.boxShadow='var(--shadow-light)';
+
+  }
+  else {
+    document.getElementsByTagName('nav')[0].style.backgroundColor='';
+    document.getElementById('Logo').style.backgroundSize='100%';
+
+    document.getElementsByTagName('nav')[0].style.boxShadow='none';
+
+  }
+}
+
+
+
+
+
+
+
+
+//auto fill booking date as today
+var date = new Date();
+var currentDate = date.toISOString().slice(0,10);
+var currentTime = '12:00';
+document.getElementById('b-date').min = currentDate;
+document.getElementById('b-date').value = currentDate;
+
+document.getElementById('b-time').value = currentTime;
+
+
+
+//review block
+let slideIndex = 2;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("review");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].classList.remove('dot-active');
+  }
+  
+  slides[slideIndex-1].style.display = 'flex';
+  dots[slideIndex-1].classList.add('dot-active');
+} 
+
+
+
+
+
